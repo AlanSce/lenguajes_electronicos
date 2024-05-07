1efeb2f73a5bcbe6129c4fa5e59d1b5918c4ac8c
Baltabuquert <166015546+Baltabuquert@users.noreply.github.com>
2024-05-06 22:51:02 -0300

    ordenar_tipo2.py

--- 02_python/01_intro/ordenar_tipo.py
+++ 02_python/01_intro/ordenar_tipo.py
@@ -2,7 +2,7 @@ import csv
 
 tipos_arboles = {}
 
-# Leer el archivo
+
 with open('arbolado-en-espacios-verdes.csv', newline='') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter=',')
 	next(spamreader)
@@ -13,14 +13,11 @@ with open('arbolado-en-espacios-verdes.csv', newline='') as csvfile:
 		else: # Es el primer arbol que leo
 			tipos_arboles[tipo_arbol] = 1
 
-# Imprimo el diccionario hasta el momento
+
 print(tipos_arboles)
 
-# Ordeno
 tipos_arboles_ordenados = {k: v for k, v in sorted(tipos_arboles.items(), key=lambda item: item[1], reverse=True)}
 
 
-# Armo la tabla ordenada
 for arbol in tipos_arboles_ordenados:
-	# Noten que la f permite agregar variables entre llaves en los prints
 	print(f'{tipos_arboles_ordenados[arbol]}  | {arbol}')
