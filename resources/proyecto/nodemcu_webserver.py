66c1f0c976bc3b8c24380dfe26a4b9ecb2fea8bf
Alan Scelza <alan.scelza@fierro.com.ar>
2024-08-06 22:30:04 -0300

    Web server

---
+++ resources/proyecto/nodemcu_webserver.py
@@ -0,0 +1,66 @@
+import network
+import urequests
+import time
+import usocket as socket
+from machine import Pin
+
+ssid = '<nombre wifi>'
+password = '<contraseña>'
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
+
+led = Pin(2, Pin.OUT)
+
+# Asegurarse que se conectó a internet
+while not station.isconnected():
+    print(".")
+    time.sleep(0.2)
+
+print(f'Connectado a wifi {ssid}')
+print(station.ifconfig())
+
+
+def web_page():
+  if led.value() == 1:
+    gpio_state="ON"
+  else:
+    gpio_state="OFF"
+  
+  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
+  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1> 
+  <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
+  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
+  return html
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 90))
+s.listen(5)
+
+print("Levantando web server")
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  request = str(request)
+  print('Content = %s' % request)
+  led_on = request.find('/?led=on')
+  led_off = request.find('/?led=off')
+  if led_on == 6:
+    print('LED ON')
+    led.value(1)
+  if led_off == 6:
+    print('LED OFF')
+    led.value(0)
+  response = web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
+
